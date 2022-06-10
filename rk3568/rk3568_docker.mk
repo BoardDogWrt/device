84edc462fe68b128130ed69e661d9b656101c59b
Lawrence-Tang <tangzongsheng@gmail.com>
2022-06-10 15:08:17 +0800

    add a variable to point to the ipk directory

--- rk3568/rk3568_docker.mk
+++ rk3568/rk3568_docker.mk
@@ -5,5 +5,5 @@
 TARGET_IMAGE_DIRNAME=friendlywrt21-docker
 TARGET_FRIENDLYWRT_CONFIG=rockchip-docker
 
-TARGET_SD_RAW_FILENAME=FriendlyWrt-21-Docker_$(date +%Y%m%d)_NanoPi-R5S_sd.img
-TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt-21-Docker_$(date +%Y%m%d)_NanoPi-R5S_eflasher.img
+TARGET_SD_RAW_FILENAME=FriendlyWrt21-Docker_$(date +%Y%m%d)_RK3568_sd.img
+TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt21-Docker_$(date +%Y%m%d)_RK3568_eflasher.img
