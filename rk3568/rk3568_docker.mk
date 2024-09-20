55e9f317c185dedefe654084031457a4cefdd338
MortyZhaoy <morty.zhaoy@gmail.com>
2024-09-20 07:25:46 -0700

    Fix docker option bug.

--- rk3568/rk3568_docker.mk
+++ rk3568/rk3568_docker.mk
@@ -3,7 +3,7 @@
 . device/boarddog/rk3568/base.mk
 
 TARGET_IMAGE_DIRNAME=boarddogwrt-docker
-TARGET_FRIENDLYWRT_CONFIG=rockchip-docker
+TARGET_BOARDDOGWRT_CONFIG=rockchip-docker
 
 TARGET_SD_RAW_FILENAME=boarddogwrt-23.05-docker_$(date +%Y%m%d)_rk3568_sd.img
 TARGET_EFLASHER_RAW_FILENAME=boarddogwrt-23.05-docker_$(date +%Y%m%d)_rk3568_eflasher.img
